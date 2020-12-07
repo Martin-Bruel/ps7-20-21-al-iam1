b3d10e231d4f3851bcdd5a1ec643362c112acdc0
Striebel <florian.striebel@live.fr>
2020-12-07 13:29:40 +0100

    #2 initialize api

--- back-end/src/main/java/restService/Server.java
+++ back-end/src/main/java/restService/Server.java
@@ -2,8 +2,7 @@ package restService;
 
 import java.util.concurrent.atomic.AtomicLong;
 
-import org.springframework.web.bind.annotation.Mapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Server {
 	private final AtomicLong counter = new AtomicLong();
 	
-	@RequestMapping("/hello-world")
+	@GetMapping("/hello-world")
 	@ResponseBody
 	public Shop shop(@RequestParam(value = "name",defaultValue = "") String name){
 		return new Shop(counter.incrementAndGet(),ShopType.COMMERCE, name, "10 avenue Tour");
