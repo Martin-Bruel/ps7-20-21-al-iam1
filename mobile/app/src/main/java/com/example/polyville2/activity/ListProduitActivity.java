f1d56e9657b955e8047d11d69115cea6886faec6
Striebel <florian.striebel@live.fr>
2020-12-18 17:14:18 +0100

    #29 publication +produit

---
+++ mobile/app/src/main/java/com/example/polyville2/activity/ListProduitActivity.java
@@ -0,0 +1,25 @@
+package com.example.polyville2.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ListView;
+
+import com.example.polyville2.R;
+import com.example.polyville2.model.Product;
+import com.example.polyville2.model.Store;
+
+public class ListProduitActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_produit);
+        Store s = (Store) getIntent().getSerializableExtra("store");
+        ListView lv = findViewById(R.id.list_view_produit);
+        ProduitAdapter p = new ProduitAdapter(s,this);
+        lv.setAdapter(p);
+
+    }
+}
