35338650b8a07c30ffa426c420733ad579b183d3
Maeva Lecavelier <maeva.lecavelier@etu.unice.fr>
2021-01-04 09:31:24 +0100

    fix weather call and clean code for release #27 - add weather call at the good time - change shop name and publications - clean code

--- store/src/main/java/dataBase/Main.java
+++ store/src/main/java/dataBase/Main.java
@@ -14,10 +14,11 @@ import weather.apis.OpenWeatherAPI;
 public class Main {
 	public static void main(String[] args) throws IOException, IncorrectUnitWeatherAPIException {
 		ArrayList<Double> address = new ArrayList<>();
+		// adresse de Polytech
 		address.add(43.61563752169879);
 		address.add(7.071778197708522);
 		OpenWeatherAPI api = new OpenWeatherAPI("metric");
-		Store shop = new Shop(0, "truc2", address, null, null, null, null, api);
+		Store shop = new Shop(0, "Café & Shop", address, null, null, null, null, api);
 	
 		shop.addProduct(new Item("item1", 13));
 
@@ -30,6 +31,14 @@ public class Main {
 		p2.addLabel(Label.COLD);
 		shop.addPublication(p2);
 
+		Publication p3 = new Publication("Lunettes de soleil", "Trop de soleil, ça pique les yeux ! N'hésitez pas à passer pour prendre des lunettes et contrer cette luminosité ! ", null);
+		p3.addLabel(Label.SUNNY);
+		shop.addPublication(p3);
+
+		Publication p4 = new Publication("Nuages : prévoyez ! ", "Avant que l'averse se déclenche n'hésitez pas à venir chercher votre parapluie !", null);
+		p4.addLabel(Label.CLOUDS);
+		shop.addPublication(p4);
+
 		List<LocalTime>hours=new ArrayList<>();
 		hours.add(LocalTime.now().withHour(8).withMinute(0));
 		hours.add(LocalTime.now().withHour(12).withMinute(0));
@@ -38,14 +47,5 @@ public class Main {
 		OpeningHours openingHours=new OpeningHours(hours,hours,hours,hours,hours,hours,hours);
 		shop.setOpeningHours(openingHours);
 		shop.makeJSON();
-		//System.out.println(shop.detailsToJSON());
-		//System.out.println(shop.toJSON());
-		//System.out.println(shop.publicationsToJSON());
-		//shop.printJSON();
-		//shop = null;
-		//System.out.println("shop"+shop);
-		//shop = JsonReader.read();
-		//System.out.println("shop"+shop);
-		//System.out.println(LocalDateTime.now());
 	}
 }
