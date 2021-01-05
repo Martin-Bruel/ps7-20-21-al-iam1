19dab171f4216ba1633827bb6949a8a39f7a32fb
LanouxNicolas <nicolas.lanoux@etu.univ-cotedazur.fr>
2021-01-05 17:37:59 +0100

    #47 ajout d'une nav barre dans activity main

---
+++ mobile/app/src/main/java/com/example/polyville2/activity/RandomActivity.java
@@ -0,0 +1,28 @@
+package com.example.polyville2.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.polyville2.R;
+import com.google.android.material.bottomnavigation.BottomNavigationItemView;
+
+public class RandomActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_random);
+
+        BottomNavigationItemView buttonOne = findViewById(R.id.action_home);
+        buttonOne.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent activity2Intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(activity2Intent);
+            }
+        });
+    }
+}
