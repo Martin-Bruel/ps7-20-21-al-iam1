39bf03d87cca3283a80113e71ecc4afb5424c0f5
Martin-Bruel <martin.bruel@etu.unice.fr>
2021-01-06 10:56:01 +0100

    fix bottombar, le magasin ne disparait plus

--- mobile/app/src/main/java/com/example/polyville2/activity/ConnectionActivity.java
+++ mobile/app/src/main/java/com/example/polyville2/activity/ConnectionActivity.java
@@ -21,8 +21,7 @@ public class ConnectionActivity extends AppCompatActivity {
         BottomNavigationItemView buttonOne = findViewById(R.id.action_home);
         buttonOne.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent activity2Intent = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(activity2Intent);
+                finish();
             }
         });
         TextView createAccount = findViewById(R.id.tv_connection_create_account);
